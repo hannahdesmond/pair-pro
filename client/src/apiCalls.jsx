49bf365b59ed45c4f0a60c5de72cfdd093f7f78b
kat-mcbrooks <katharinebrooks@hotmail.co.uk>
2022-03-10 09:05:38 +0000

    fixed login in usercontroller

--- client/src/apiCalls.jsx
+++ client/src/apiCalls.jsx
@@ -2,10 +2,17 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import { Navigate } from "react-router-dom";
 
-export const loginCall = async (userCredentials, dispatch) => {
+export const loginCall = async (
+  { email: email, password: password },
+  dispatch
+) => {
+  console.log({ email: email, password: password });
   dispatch({ type: "LOGIN_START" });
   try {
-    const res = await axios.post("/api/users/login", userCredentials);
+    const res = await axios.post("/api/users/login", {
+      email: email,
+      password: password,
+    });
     console.log(res.data);
     localStorage.setItem("user", JSON.stringify(res.data));
     localStorage.setItem("userToken", res.data.token);
@@ -20,10 +27,10 @@ export const loginCall = async (userCredentials, dispatch) => {
 export const registerCall = async (userCredentials, dispatch) => {
   dispatch({ type: "LOGIN_START" });
   try {
-    const res = await axios.post(`/api/users/`, userCredentials)
+    const res = await axios.post(`/api/users/`, userCredentials);
     localStorage.setItem("user", JSON.stringify(res.data));
     localStorage.setItem("userToken", res.data.token);
-    console.log(res.data.token)
+    console.log(res.data.token);
     dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
     // <Navigate to="/" replace={true} />; //this doesn't seem to take precedenc over the redirect in the app.js
   } catch (err) {
